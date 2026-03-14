f1077747a841cc6cbda1684f88a8a78bfefded9d
Howard Chan <lonely829@gmail.com>
2026-03-14 16:53:23 +0800

    add test ask user tool

---
+++ tests/test_ask_user_tool.py
@@ -0,0 +1,10 @@
+import pytest
+from tools.ask_user_tool import AskUserTool
+
+@pytest.mark.skip
+def test_ask_user_tool():
+    ask_user_tool = AskUserTool()
+    func_args = '{"question": "你今天开心吗？", "options": ["开心", " 不开心"]}'
+    result = ask_user_tool.call(func_args=func_args)
+    assert result.is_error is False
+    
